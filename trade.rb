23a841be46d279e349d9231037b0c5944e59c4b7
Blaine Pace <blainepace@gmail.com>
2019-05-18 16:01:55 -0700

    Refactor procedural code into object

--- trade.rb
+++ trade.rb
@@ -3,23 +3,37 @@
 require 'json'
 require 'pp'
 
-File.open("data/commodities.json") do |f|
-  puts "Parsing commodities.json into JSON..\n\n"
-  @commodities = Hash.new
-  JSON.parse(f.read).each do |c|
-    buy_price, sell_price = c['min_buy_price'], c['max_sell_price']
-    unless buy_price.nil? or sell_price.nil?
-      profit = sell_price - buy_price
-      @commodities[profit] = c
-      #pp @commodities.sort
+class Commodities
+  def initialize
+    @commodities = Hash.new
+  end
+
+  def open_and_parse
+    File.open("data/commodities.json") do |f|
+      puts "Parsing commodities.json into JSON..\n\n"
+      @commodities = Hash.new
+      JSON.parse(f.read).each do |c|
+        buy_price, sell_price = c['min_buy_price'], c['max_sell_price']
+        unless buy_price.nil? or sell_price.nil?
+          profit = sell_price - buy_price
+          @commodities[profit] = c
+        end
+      end
     end
   end
-  for c in @commodities.sort.reverse[0..10]
-    j = c[1]
-    puts j['name']
-    puts "Profit: #{c[0]}"
-    puts "Minimum Buy Price: #{j['min_buy_price']}"
-    puts "Maximum Sell Price: #{j['max_sell_price']}"
-    puts "\n"
+
+  def print_top_10
+    for c in @commodities.sort.reverse[0..10]
+      j = c[1]
+      puts j['name']
+      puts "Profit: #{c[0]}"
+      puts "Minimum Buy Price: #{j['min_buy_price']}"
+      puts "Maximum Sell Price: #{j['max_sell_price']}"
+      puts "\n"
+    end
   end
 end
+
+c = Commodities.new
+c.open_and_parse
+c.print_top_10
