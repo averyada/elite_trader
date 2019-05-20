7d1e7f273492742d99379923b9d3a4cf7a3db869
Blaine Pace <blainepace@gmail.com>
2019-05-19 22:32:04 -0700

    Add listings.sql for loading listings.csv into a database

---
+++ listings.sql
@@ -0,0 +1,17 @@
+.mode csv
+.open listings.db
+BEGIN;
+CREATE TABLE listings(
+  "id" INTEGER NOT NULL,
+  "station_id" INTEGER NOT NULL,
+  "commodity_id" INTEGER NOT NULL,
+  "supply" INTEGER NOT NULL,
+  "supply_bracket" INTEGER NOT NULL,
+  "buy_price" INTEGER NOT NULL,
+  "sell_price" INTEGER NOT NULL,
+  "demand" INTEGER NOT NULL,
+  "demand_bracket" INTEGER NOT NULL,
+  "collected_at" INTEGER NOT NULL
+);
+COMMIT;
+.import data/listings.csv listings
