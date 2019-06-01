4edd466f186b36381460c1cf97a06bedbe03c020
Blaine Pace <blainepace@gmail.com>
2019-06-01 08:58:06 -0700

    Parse stations.json and look up relevant buy and sell stations

--- trade.rb
+++ trade.rb
@@ -3,6 +3,30 @@ require 'csv'
 require 'sqlite3'
 require 'pp'
 
+class Stations
+  def initialize
+    @stations = Hash.new
+  end
+
+  def open_and_parse
+    File.open("data/stations.json") do |f|
+      puts "\nParsing stations.json into JSON..\n\n"
+      JSON.parse(f.read).each do |c|
+        station_id = c['id']
+        @stations[station_id] = c
+      end
+    end
+  end
+
+  def find(station_search_id)
+    station = @stations.fetch(station_search_id)
+    puts "\nStation Name     : #{station['name']}"
+    puts "Landing Pad Size   : #{station['max_landing_pad_size']}"
+    puts "Distance from star : #{station['distance_to_star']} Ls"
+    return station['system_id']
+  end
+end
+
 class Commodities
   def initialize
     @commodities = Hash.new
@@ -10,8 +34,7 @@ class Commodities
 
   def open_and_parse
     File.open("data/commodities.json") do |f|
-      puts "Parsing commodities.json into JSON..\n\n"
-      @commodities = Hash.new
+      puts "\nParsing commodities.json into JSON..\n\n"
       JSON.parse(f.read).each do |c|
         buy_price, sell_price = c['min_buy_price'], c['max_sell_price']
         unless buy_price.nil? or sell_price.nil?
@@ -60,12 +83,21 @@ class Listings
     WHERE commodity_id == #{commodity_search_id}
     ORDER BY sell_price DESC
     SQL
-    lowest_buy_price = @db.execute(buy_price_sql)[0][5]
-    highest_sell_price = @db.execute(sell_price_sql)[0][6]
+
+    lowest_buy_price_data   = @db.execute(buy_price_sql)[0]
+    highest_sell_price_data = @db.execute(sell_price_sql)[0]
+
+    lowest_buy_price   = lowest_buy_price_data[5]
+    highest_sell_price = highest_sell_price_data[6]
+    buy_station_id     = lowest_buy_price_data[1]
+    sell_station_id    = highest_sell_price_data[1]
+
     puts "-------------------------------------------"
     puts "Lowest buy price   : #{lowest_buy_price} CR"
     puts "Highest sell price : #{highest_sell_price} CR"
     puts "Profit per trip    : #{highest_sell_price - lowest_buy_price} CR"
+
+    return buy_station_id, sell_station_id
   end
 end
 
@@ -74,4 +106,9 @@ c.open_and_parse
 c.print_top_10
 
 l = Listings.new
-l.find(c.first_result)
+buy_station_id, sell_station_id = l.find(c.first_result)
+
+s = Stations.new
+s.open_and_parse
+s.find(buy_station_id)
+s.find(sell_station_id)
