4431d33a978ef09d6ecbd011fbcd250bdd140788
Blaine Pace <blainepace@gmail.com>
2019-06-01 13:41:35 -0700

    Encapsulated trade hop logic inside TradeHopFinder class

--- trade.rb
+++ trade.rb
@@ -12,6 +12,58 @@ COMMODITIES       = "data/commodities.json"
 # Database
 LISTINGS_DB       = "listings.db"
 
+class TradeHopFinder
+  attr_reader :systems, :stations, :commodities, :listings
+  def initialize
+    @systems     = Systems.new
+    @stations    = Stations.new
+    @commodities = Commodities.new
+    @listings    = Listings.new
+  end
+
+  def top_profitable_commodity
+    return @commodities.first_result
+  end
+
+  def find_best_single_hop
+    commodity_id = @commodities.first_result['id']
+    buy_station_id, sell_station_id = @listings.find(commodity_id)
+
+    buy_station  = @stations.find(buy_station_id)
+    sell_station = @stations.find(sell_station_id)
+    buy_system   = @systems.find(buy_station['system_id'])
+    sell_system  = @systems.find(sell_station['system_id'])
+
+    puts
+    puts "-- Purchase commodity from --"
+    print_system_info(buy_system)
+    print_station_info(buy_station)
+
+    puts
+    puts "-- Sell commodity at --"
+    print_system_info(sell_system)
+    print_station_info(sell_station)
+  end
+
+  private
+  def print_system_info(system)
+    puts "System Name  : #{system['name']}"
+    puts "Allegiance   : #{system['allegiance']}"
+    puts "Security     : #{system['security']}"
+    puts "Needs permit : #{system['needs_permit']}"
+    puts "X coordinate : #{system['x']}"
+    puts "Y coordinate : #{system['y']}"
+  end
+
+  def print_station_info(station)
+    puts "Station Name       : #{station['name']}"
+    puts "Landing Pad Size   : #{station['max_landing_pad_size']}"
+    puts "Distance from star : #{station['distance_to_star']} Ls"
+    puts "Market updated at  : #{Time.at(station['market_updated_at'])}"
+    puts "Planetary station  : #{station['is_planetary']}"
+  end
+end
+
 class Systems
   def initialize
     @systems = Hash.new
@@ -25,13 +77,7 @@ class Systems
   end
 
   def find(system_search_id)
-    system = @systems.fetch(system_search_id)
-    puts "System Name  : #{system['name']}"
-    puts "Allegiance   : #{system['allegiance']}"
-    puts "Security     : #{system['security']}"
-    puts "Needs permit : #{system['needs_permit']}"
-    puts "X coordinate : #{system['x']}"
-    puts "Y coordinate : #{system['y']}"
+    return @systems.fetch(system_search_id)
   end
 end
 
@@ -48,13 +94,7 @@ class Stations
   end
 
   def find(station_search_id)
-    station = @stations.fetch(station_search_id)
-    puts "Station Name       : #{station['name']}"
-    puts "Landing Pad Size   : #{station['max_landing_pad_size']}"
-    puts "Distance from star : #{station['distance_to_star']} Ls"
-    puts "Market updated at  : #{Time.at(station['market_updated_at'])}"
-    puts "Planetary station  : #{station['is_planetary']}"
-    return station['system_id']
+    return @stations.fetch(station_search_id)
   end
 end
 
@@ -88,7 +128,7 @@ class Commodities
   end
 
   def first_result
-    @commodities[0][1]['id']
+    @commodities[0][1]
   end
 end
 
@@ -120,35 +160,14 @@ class Listings
     highest_sell_price = highest_sell_price_data[6]
     buy_station_id     = lowest_buy_price_data[1]
     sell_station_id    = highest_sell_price_data[1]
-
-    puts "-------------------------------------------"
-    puts "Lowest buy price   : #{lowest_buy_price} CR"
-    puts "Highest sell price : #{highest_sell_price} CR"
-    puts "Profit per trip    : #{highest_sell_price - lowest_buy_price} CR"
-    puts "-------------------------------------------"
-
-
+    
     return buy_station_id, sell_station_id
   end
 end
 
-systems = Systems.new
-stations = Stations.new
-commodities = Commodities.new
-listings = Listings.new
+tradehops = TradeHopFinder.new
 
 puts "-------------------------------------------"
 
-commodities.print_top_10
-
-buy_station_id, sell_station_id = listings.find(commodities.first_result)
-
-puts
-puts "-- Purchase commodity from --"
-buy_system_id  = stations.find(buy_station_id)
-systems.find(buy_system_id)
-
-puts
-puts "-- Sell commodity at --"
-sell_system_id = stations.find(sell_station_id)
-systems.find(sell_system_id)
+tradehops.commodities.print_top_10
+tradehops.find_best_single_hop()
