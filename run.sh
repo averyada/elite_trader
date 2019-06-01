755d365db9c60e34ae1d738ef0f6124b77eebc22
Blaine Pace <blainepace@gmail.com>
2019-06-01 09:35:47 -0700

    Add run.sh to handle database init and script execution

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DATABASE=listings.db
+
+cd "${0%/*}"
+
+if test -f "$DATABASE"; then
+  echo "Commodity listings database already exists.."
+else
+  echo "Initializing commodity database listings.db from csv file.."
+  cat listings.sql | sqlite3
+fi
+ruby trade.rb
