39a4617681a309fe4958f9148298a5d4fd0bcc4a
Blaine Pace <blainepace@gmail.com>
2019-06-02 07:32:45 -0700

    Rename SQL heredocs in Listings class to be more descriptive

--- lib/trade/listings.rb
+++ lib/trade/listings.rb
@@ -13,19 +13,19 @@ class Listings
   end
 
   def find(commodity_search_id)
-    buy_price_sql = <<~SQL
+    buy_listings_sql = <<~SQL
     SELECT * FROM listings
     WHERE commodity_id == #{commodity_search_id}
     ORDER BY buy_price=0, buy_price ASC
     SQL
 
-    sell_price_sql = <<~SQL
+    sell_listings_sql = <<~SQL
     SELECT * FROM listings
     WHERE commodity_id == #{commodity_search_id}
     ORDER BY sell_price DESC
     SQL
 
-    @buy_stations  = @db.execute(buy_price_sql)
-    @sell_stations = @db.execute(sell_price_sql)
+    @buy_stations  = @db.execute(buy_listings_sql)
+    @sell_stations = @db.execute(sell_listings_sql)
   end
 end
